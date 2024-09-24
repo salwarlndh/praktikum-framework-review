ceb8916fe685de85057dbe31d2f6ed05b57baffb
Salwa Arlinda Humairah <126219178+salwarlndh@users.noreply.github.com>
2024-09-24 09:00:03 +0800

    Review3

--- framework/ais/admin.py
+++ framework/ais/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.hashers import make_password # Untuk hashing password
+from .models.teachers import Teachers
+from .models.students import Students
+from .models.users import Users
 
-# Register your models here.
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('nip', 'name', 'email', 'phone_number')
+
+    def save_model(self, request, obj, form, change):
+    # Simpan Teacher terlebih dahulu
+        super().save_model(request, obj, form, change)
+
+    # Cek apakah user dengan email guru sudah ada
+        user, created = Users.objects.get_or_create(username=obj.nip, defaults={ # Pastikan ini model Users kustom
+        'password': make_password('default_password'), # Menggunakan hashing password
+        'role': Users.TEACHER # Pastikan Anda memiliki field role di model Users
+        })
+
+        if not created:
+            # Jika user sudah ada, perbarui role (untuk berjaga-jaga)
+            user.role = Users.TEACHER
+            user.save()
+
+# Daftarkan Teachers dengan custom TeacherAdmin
+admin.site.register(Teachers, TeacherAdmin)
+# Daftarkan Model Students dan Users
+admin.site.register(Students)
+admin.site.register(Users)
--- framework/framework/settings.py
+++ framework/framework/settings.py
@@ -65,6 +65,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.static",
             ],
         },
     },
@@ -123,6 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
