b426cb899b96b77ee8bd8e71c7d0f4e3eae12192
Salwa Arlinda Humairah <126219178+salwarlndh@users.noreply.github.com>
2024-10-01 18:51:46 +0800

    Review4

--- framework/ais/urls.py
+++ framework/ais/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('about', views.about, name='about'),
     path('', views.homepage),
+    path('student/', views.student_index, name='student_index'), # Read
+    path('student/create/', views.student_create, name='student_create'),# Create
 ]
--- framework/ais/views.py
+++ framework/ais/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Students
+from django.contrib import messages
+from .forms import StudentsForm
 
 # Create your views here.
 def homepage(request):
@@ -6,3 +9,20 @@ def homepage(request):
 
 def about(request):
     return render(request, 'homepage/about.html')
+
+# READ Mahasiswa
+def student_index(request):
+    students = Students.objects.all()
+    return render(request, 'student/index.html', {'students': students})
+
+# CREATE Mahasiswa
+def student_create(request):
+    if request.method == 'POST':
+        form = StudentsForm(request.POST)
+        if form.is_valid():
+            form.save() # Simpan data mahasiswa ke database
+            messages.success(request, 'Mahasiswa berhasil dibuat!') # Pesan sukses
+            return redirect('student_index') # Redirect ke halaman index mahasiswa
+    else:
+        form = StudentsForm()
+    return render(request, 'student/create.html', {'form': form})
